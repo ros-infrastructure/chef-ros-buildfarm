586f2261fc8685ca7edb27edf96ba49dd4e480f7
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-08-28 13:37:22 -0400

    Agents should connect to Jenkins on port 8080 to skip the web proxy.

--- environments/test.rb
+++ environments/test.rb
@@ -2,7 +2,7 @@ name 'test'
 description 'Test deployment environment'
 default_attributes Hash[
   ros_buildfarm: {
-    jenkins_url: "http://testbuild.rosbuild.ros2.org/",
+    jenkins_url: "http://testbuild.rosbuild.ros2.org:8080/",
     agent: {
       username: 'admin',
       password: 'this is a test',
