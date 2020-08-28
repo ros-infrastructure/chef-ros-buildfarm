59af868369d643ec8d4befe9fffbe0b545576182
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-08-27 23:31:04 -0400

    Use domain name instead of IP.
    This domain is provided by a private DNS zone in our virtual private
    network.

--- environments/test.rb
+++ environments/test.rb
@@ -2,7 +2,7 @@ name 'test'
 description 'Test deployment environment'
 default_attributes {
   ros_buildfarm: {
-    jenkins_url: "http://100.21.122.128/",
+    jenkins_url: "http://rosbuild.test.ros2.org/",
     agent: {
       username: 'admin',
       password: 'this is a test',
