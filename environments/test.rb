1fb5b673e2264e265ca3ad2174763906cef05b9a
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-08-28 13:14:21 -0400

    Update domains used in test deployment.

--- environments/test.rb
+++ environments/test.rb
@@ -2,13 +2,13 @@ name 'test'
 description 'Test deployment environment'
 default_attributes Hash[
   ros_buildfarm: {
-    jenkins_url: "http://rosbuild.test.ros2.org/",
+    jenkins_url: "http://testbuild.rosbuild.ros2.org/",
     agent: {
       username: 'admin',
       password: 'this is a test',
     },
     ssh_publisher: {
-      repo_hostname: 'repositories.test.ros2.org',
+      repo_hostname: 'repositories.rosbuild.ros2.org',
     },
   }
 ]
