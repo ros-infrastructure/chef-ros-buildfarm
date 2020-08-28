16d6ffd8397f64585f844e7ed91d44909b69054b
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-08-27 23:20:57 -0400

    Add test-environment specific attributes for ros_buildfarm.

--- environments/test.rb
+++ environments/test.rb
@@ -1,3 +1,16 @@
 name 'test'
 description 'Test deployment environment'
+default_attributes {
+  ros_buildfarm: {
+    jenkins_url: "http://100.21.122.128/",
+    agent: {
+      username: 'admin',
+      password: 'this is a test',
+    }
+    ssh_publisher: {
+      repo_hostname: 'repositories.test.ros2.org',
+    }
+  }
+}
+
 
